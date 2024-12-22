492c9bd4baec1c161cf82186ba169db3ddaa1e15
Dean Sas <dean@deansas.org>
2024-12-22 15:52:10 +0000

    Add wpcom-sync-once alias

--- .zsh-custom/a8c-alias.zsh
+++ .zsh-custom/a8c-alias.zsh
@@ -1,5 +1,6 @@
 # Sync my wp.com sandbox
 alias wpcom-sync='unison -ui text -repeat watch wpcom'
+alias wpcom-sync-once='unison -ui text wpcom'
 alias mc-sync='unison -ui text -repeat watch missioncontrol'
 
 alias pnpm='npx pnpm'
