522ae80e3d6b9350f9b1e36ae018eb660a24237c
Dean Sas <dean@deansas.org>
2026-01-16 17:20:59 +0000

    Remove unused sync aliases

--- .zsh-custom/a8c-alias.zsh
+++ .zsh-custom/a8c-alias.zsh
@@ -1,8 +1,3 @@
-# Sync my wp.com sandbox
-alias wpcom-sync='unison -ui text -repeat watch wpcom'
-alias wpcom-sync-once='unison -ui text wpcom'
-alias mc-sync='unison -ui text -repeat watch missioncontrol'
-
 alias pnpm='npx pnpm'
 
 # Sync local gb to wpcom sandbox
