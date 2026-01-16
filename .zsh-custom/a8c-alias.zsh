a057e794d112f2aebae1bdcf645b4ab2a4cac8bd
Dean Sas <dean@deansas.org>
2026-01-16 16:49:30 +0000

    Add alias to sync gb to wpcom-sandbox

--- .zsh-custom/a8c-alias.zsh
+++ .zsh-custom/a8c-alias.zsh
@@ -4,3 +4,6 @@ alias wpcom-sync-once='unison -ui text wpcom'
 alias mc-sync='unison -ui text -repeat watch missioncontrol'
 
 alias pnpm='npx pnpm'
+
+# Sync local gb to wpcom sandbox
+alias gbsync="rsync -rvahz . wpcom-sandbox:/home/wpcom/public_html/wp-content/plugins/gutenberg-core/dev/ --include-from=$HOME/bin/gb-sync/include --exclude-from=$HOME/bin/gb-sync/exclude"
