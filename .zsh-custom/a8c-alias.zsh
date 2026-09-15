911c10b8bbede1c3796f23c12b08fdd2fc9c95de
Dean Sas <dean@deansas.org>
2026-09-15 13:49:18 +0100

    Stop using the pnpm alias

--- .zsh-custom/a8c-alias.zsh
+++ .zsh-custom/a8c-alias.zsh
@@ -1,4 +1,2 @@
-alias pnpm='npx pnpm'
-
 # Sync local gb to wpcom sandbox
 alias gbsync="rsync -rvahz . wpcom-sandbox:/home/wpcom/public_html/wp-content/plugins/gutenberg-core/dev/ --include-from=$HOME/bin/gb-sync/include --exclude-from=$HOME/bin/gb-sync/exclude"
